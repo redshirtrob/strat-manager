5804a9b96f7b08755d1a4880e74c4d86e55ba87c
Robert S. Jones <robert.jones.sv@gmail.com>
2015-08-05 21:07:12 -0700

    Extract all attachments from ms-tnef data.

--- extractor.py
+++ extractor.py
@@ -8,59 +8,20 @@ import os
 import quopri
 import tnefparse
 
-UNKNOWN_COUNT=0
-
 def extract_html_from_text_html(part):
-    html = part.get_payload(decode=True)
-    return html
+    return [part.get_payload(decode=True)]
 
 def extract_html_from_application_ms_tnef(message=None, part=None):
-    html = None
-
     cte = part.get('Content-Transfer-Encoding')
     if cte == 'quoted-printable':
         decoded_part = quopri.decodestring(part.get_payload())
-        tnef = tnefparse.TNEF(decoded_part)
-        if len(tnef.attachments) == 0:
-            print "Didn't find any attachments: {}".format(len(tnef.attachments))
-        elif len(tnef.attachments) > 1:
-            print "Found more than one attatchment: {}".format(len(tnef.attachments))
-        else:
-            attachment = tnef.attachments[0]
-            html = attachment.data
     elif cte == 'base64':
         decoded_part = base64.b64decode(part.get_payload())
-        tnef = tnefparse.TNEF(decoded_part)
-        if len(tnef.attachments) == 0:
-            print "Didn't find any attachments: {}".format(len(tnef.attachments))
-        elif len(tnef.attachments) > 1:
-            print "Found more than one attatchment: {}".format(len(tnef.attachments))
-        else:
-            attachment = tnef.attachments[0]
-            html = attachment.data
     else:
         print "Don't know how to decode: '{}'".format(cte)
         
-    return html
-
-def gen_filename(subject, count):
-    global UNKNOWN_COUNT
-
-    try:
-        _, date = subject.split()
-        m, d, y = map(int, date.split('/'))
-        d = datetime.datetime(y+2000, m, d)
-        base = '{:%Y%m%d}'.format(d)
-    except ValueError as e:
-        base = 'UNKNOWN{}'.format(UNKNOWN_COUNT)
-        UNKNOWN_COUNT += 1
-        
-    if count > 0:
-        filename = '{}{}DAY.htm'.format(base, count)
-    else:
-        filename = '{}DAY.htm'.format(base)
-
-    return filename
+    tnef = tnefparse.TNEF(decoded_part)
+    return [attachment.data for attachment in tnef.attachments]
 
 def get_title(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -72,15 +33,18 @@ def get_title(html):
 def main():
     mbox = mailbox.mbox('BLB-Sirk.mbox')
 
+    total_attachment_count = 0
+    attachment_count = 0
     current_message_index = 0
     for message in mbox:
         subject = message['subject']
-        ident = message['Message-ID']
+        message_id = message['Message-ID']
 
-        if subject.lower().startswith('re'):
+        # Skip replies
+        if subject.lower().startswith('re:'):
             continue
         
-        print "Processing: {}".format(ident)
+        print "Processing: {}".format(message_id)
 
         count = 0
         parts = message.get_payload()
@@ -88,34 +52,33 @@ def main():
             if isinstance(part, str):
                 continue
 
-            filename = html = None
+            attachments = []
             content_type = part.get_content_type()
             if content_type == 'text/html':
-                filename = part.get_filename()
-                if filename is None:
-                    filename = gen_filename(subject, count)
-                html = extract_html_from_text_html(part)
-                print "Extracted {} file: {}".format(content_type, filename)
+                attachments = extract_html_from_text_html(part)
                 count += 1
             elif content_type == 'application/ms-tnef':
-                filename = gen_filename(subject, count)
-                html = extract_html_from_application_ms_tnef(message, part)
-                print "Extracted {} file: {}".format(content_type, filename)
+                attachments = extract_html_from_application_ms_tnef(message, part)
                 count += 1
             else:
                 if content_type != 'text/plain' and content_type != 'multipart/alternative':
                     print "Skipping: {}".format(content_type)
 
-            if html is not None:
-                print "Title: {}".format(get_title(html))
+            for attachment in attachments:
+                print "Title: {}".format(get_title(attachment))
+                filename = '{}.dat'.format(attachment_count)
                 full_path = os.path.join('data-files', filename)
                 with open(full_path, 'w') as f:
-                    f.write(html)
-
+                    f.write(attachment)
+                attachment_count += 1
+            total_attachment_count += len(attachments)
+                
         if count == 0:
             print "ERROR: Couldn't extract data from {}".format(subject)
         else:
             print "Extracted {} data file(s) from {}".format(count, subject)
+        current_message_index += 1
+    print "Extracted {} attachments from data set".format(total_attachment_count)
             
 if __name__ == '__main__':
     main()
